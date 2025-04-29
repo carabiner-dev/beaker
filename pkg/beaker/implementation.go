27249c04fd65ee0df329a5fdf1c0e06cb718d6f0
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-04-29 14:54:54 -0600

    Add beaker implementation
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ pkg/beaker/implementation.go
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+
+package beaker
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	"github.com/carabiner-dev/beaker/internal/git"
+	v0 "github.com/in-toto/attestation/go/predicates/test_result/v0"
+	v1 "github.com/in-toto/attestation/go/v1"
+	"sigs.k8s.io/release-utils/util"
+)
+
+type launcherImplementation interface {
+	InitAttestation(context.Context, *Options) (*v0.TestResult, error)
+	RunLaunchPack(context.Context, *Options, *LaunchPack) ([]byte, error)
+}
+
+type defaultLauncherImplementation struct{}
+
+func (dli *defaultLauncherImplementation) RunLaunchPack(ctx context.Context, opts *Options, pack *LaunchPack) ([]byte, error) {
+	output, _, err := pack.Runner.Run(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("runner error: %w", err)
+	}
+	return output, nil
+}
+
+func (dli *defaultLauncherImplementation) InitAttestation(_ context.Context, opts *Options) (*v0.TestResult, error) {
+	if !util.Exists(filepath.Join(opts.WorkDir, ".git")) {
+		return nil, nil
+	}
+
+	// Get the repo version
+	tagPlus, commit, err := git.RepoVersion(opts.WorkDir)
+	if err != nil {
+		return nil, fmt.Errorf("computing git commit: %w", err)
+	}
+
+	// Build the attestation
+	att := &v0.TestResult{
+		Configuration: []*v1.ResourceDescriptor{
+			{
+				Name: tagPlus,
+				//Uri:  "",
+				Digest: map[string]string{
+					"sha1": commit,
+				},
+			},
+		},
+		// Url: "",
+	}
+	return att, nil
+}
