ea4991c6d45b8857cbb10468ed0767c9b47de434
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-04-29 15:11:06 -0600

    Import some of bnd git functions
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- internal/git/git.go
+++ internal/git/git.go
@@ -4,13 +4,16 @@
 package git
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"sigs.k8s.io/release-utils/command"
 )
 
 func getHeadHash(repo *git.Repository) (string, error) {
@@ -162,4 +165,102 @@ func getLatestTagFromRepository(repo *git.Repository) (string, *object.Commit, e
 	}
 
 	return strings.TrimPrefix(latestTagName, "refs/tags/"), latestTagCommit, nil
+}
+
+// TODO(puerco): Port this to pure go before making it public
+func GetRemotes(path string) (map[string]string, error) {
+	remotes := map[string]string{}
+	res, err := command.NewWithWorkDir(path, "git", "remote", "-v").RunSilentSuccessOutput()
+	if err != nil {
+		return nil, fmt.Errorf("running git to get remotes: %w", err)
+	}
+	remoteout := res.Output()
+
+	scanner := bufio.NewScanner(strings.NewReader(remoteout))
+	for scanner.Scan() {
+		pts := strings.Fields(scanner.Text())
+		if len(pts) < 2 {
+			continue
+		}
+		remotes[pts[0]] = pts[1]
+	}
+	return remotes, nil
+}
+
+func RepoVCSLocator(path string) (string, error) {
+	// PArse some head details:
+	head, err := GetHeadDetails(path)
+	if err != nil {
+		return "", fmt.Errorf("getting head details: %w", err)
+	}
+
+	// Read the remotes from the repo
+	remotes, err := GetRemotes(path)
+	if err != nil {
+		return "", fmt.Errorf("reading remotes: %w", err)
+	}
+	return makeVCSLocator(head, remotes)
+}
+
+type HeadDetails struct {
+	CommitSHA string
+	Tag       string
+}
+
+// TODO(puerco): Port this to pure go before making it public
+func GetHeadDetails(path string) (*HeadDetails, error) {
+	details := &HeadDetails{}
+
+	res, err := command.NewWithWorkDir(path, "git", "rev-parse", "HEAD").RunSilentSuccessOutput()
+	if err != nil {
+		return nil, fmt.Errorf("running git to get revision: %w", err)
+	}
+	details.CommitSHA = res.OutputTrimNL()
+
+	res, err = command.NewWithWorkDir(path, "git", "tag", "--points-at", "HEAD").RunSilentSuccessOutput()
+	if err != nil {
+		return nil, fmt.Errorf("running git to get revision: %w", err)
+	}
+	details.Tag = res.OutputTrimNL()
+
+	return details, nil
+}
+
+func makeVCSLocator(head *HeadDetails, remotes map[string]string) (string, error) {
+	sourceURL := ""
+
+	pref := []string{"upstream", "origin"}
+	for _, k := range pref {
+		if v, ok := remotes[k]; ok {
+			sourceURL = v
+			break
+		}
+	}
+
+	// If no match, pick the first
+	if sourceURL == "" {
+		for _, v := range remotes {
+			sourceURL = v
+			break
+		}
+	}
+
+	// If the URL is on ssh, we need to make some changes
+	if strings.Contains(sourceURL, "@") {
+		_, u, _ := strings.Cut(sourceURL, "@")
+		u = strings.Replace(u, ":", "/", 1)
+
+		// if its a github url, remote the .git to make it more compact
+		if strings.HasPrefix(u, "github.com/") {
+			u = strings.TrimSuffix(u, ".git")
+		}
+		sourceURL = "ssh://" + u
+	}
+
+	u, err := url.Parse(sourceURL)
+	if err != nil {
+		return "", fmt.Errorf("parsing repo source URL")
+	}
+
+	return "git+" + u.String() + "@" + head.CommitSHA, nil
 }
